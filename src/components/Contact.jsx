c45a839fb5480f591535164dcea04279bab4eed1
sapana <sapanadhami1111@gmail.com>
2026-01-16 21:54:37 +0000

    Complete app

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,11 +22,7 @@ const Contact = () => {
 		.from('#contact h3, #contact p', {
 			opacity: 0, yPercent: 100, stagger: 0.02
 	 })
-		.to('#f-right-leaf', {
-		 y: '-50', duration: 1, ease: 'power1.inOut'
-	 }).to('#f-left-leaf', {
-		 y: '-50', duration: 1, ease: 'power1.inOut'
-	 }, '<')
+		
 	})
  
  return (
